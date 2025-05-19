8f4af16b6e205b89bba04cc32a61db9631a367f5
pedrooayres <pedrohayres@yahoo.com.br>
2025-05-19 17:29:36 +0000

    defenindo a estrutura do token + mais palavras reservadas + criando o scanner: implmenta a logica do analisador lexico + main :  executa o scanner sobre um arquivo passado por argumento.

--- lexer_rules.py
+++ lexer_rules.py
@@ -1,3 +1,4 @@
+# lexer_rules.py
 RESERVADAS = {"main", "int", "float", "char", "if", "else", "while", "do", "for"}
 OPERADORES_ARIT = {"+", "-", "*", "/"}
 OPERADORES_REL = {"<", "<=", ">", ">=", "==", "!="}
--- main.py
+++ main.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Uso: python main.py <arquivo>")
         exit(1)
-    
+
     scanner = Scanner(sys.argv[1])
     try:
         tokens = scanner.tokenizar()
         for token in tokens:
             print(token)
-        print("Compilação concluída com sucesso.")
+        print("Compilacao concluida com sucesso.")
     except Exception as e:
-        print(f"Erro na análise léxica: {e}")
+        print(f"Erro na analise lexica: {e}")
