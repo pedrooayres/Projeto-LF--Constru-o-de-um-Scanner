913adc06e21c9d0edb16570f3e8c4b9f505d521a
pedrooayres <pedrohayres@yahoo.com.br>
2025-05-20 17:26:53 +0000

    tabela de simbolos

---
+++ tabela_simbolos.py
@@ -0,0 +1,28 @@
+# tabela_simbolos.py
+
+class Simbolo:
+    def __init__(self, lexema, tipo, escopo):
+        self.lexema = lexema      # Nome da variável
+        self.tipo = tipo          # Tipo da variável (int, float, char, etc.)
+        self.escopo = escopo      # Nível de escopo em que a variável foi declarada
+
+class TabelaSimbolos:
+    def __init__(self):
+        self.tabela = []
+
+    def inserir(self, simbolo):
+        # Verifica se já existe símbolo com mesmo nome no mesmo escopo
+        if any(s.lexema == simbolo.lexema and s.escopo == simbolo.escopo for s in self.tabela):
+            raise Exception(f"Variável '{simbolo.lexema}' já declarada neste escopo")
+        self.tabela.append(simbolo)
+
+    def buscar(self, lexema):
+        # Busca do escopo mais interno para o mais externo
+        for simbolo in reversed(self.tabela):
+            if simbolo.lexema == lexema:
+                return simbolo
+        return None
+
+    def remover_escopo(self, escopo):
+        # Remove todas as variáveis do escopo atual ao sair de um bloco
+        self.tabela = [s for s in self.tabela if s.escopo < escopo]
